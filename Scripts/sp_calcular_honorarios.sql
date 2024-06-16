8c36d1f8490ae4a9c9e6754220e66e68a0112ce8
Emi Vargas <emivargasutd@gmail.com>
2024-06-16 12:54:28 -0300

    modifico modo de calculo en sp_calcular_honorarios.sql

--- Scripts/sp_calcular_honorarios.sql
+++ Scripts/sp_calcular_honorarios.sql
@@ -4,10 +4,9 @@ delimiter //
 drop procedure if exists sp_calcula_honorarios//
 create procedure sp_calcula_honorarios(in i_dia varchar(50), in i_id_medico int, out rta decimal(10, 2))
 begin
-    set rta = (SELECT sum(pr.costo)
+    set rta = (SELECT sum(tu.costoFinal)
                FROM turno tu
                INNER JOIN medico me on me.idMedico = tu.id_medico
-               INNER JOIN practica pr on pr.idPractica = tu.id_practica
                WHERE tu.fechaAtencion = i_dia AND me.idMedico = i_id_medico AND tu.acreditado = 1);
 end //
 delimiter ;
